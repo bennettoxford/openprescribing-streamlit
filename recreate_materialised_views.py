7002e1dac83f9b7889da47272e5f6d431a180f1b
Tom Ward <tomward@fmail.co.uk>
2026-05-21 15:27:32 +0100

    Script to (re-)create all materialised views

---
+++ recreate_materialised_views.py
@@ -0,0 +1,28 @@
+import logging
+
+from db import (
+    create_materialised_view,
+    materialised_views_db_path,
+    materialised_views_dir,
+)
+
+logger = logging.getLogger(__name__)
+
+
+def recreate_materialised_views():
+    logging.debug("Removing all materialised views")
+    if materialised_views_db_path.exists():
+        materialised_views_db_path.unlink()
+
+    logging.debug("Creating all materialised views")
+    for f in materialised_views_dir.iterdir():
+        short_name = f.name.removesuffix(".sql")
+        logging.debug(f"Creating materialised view {short_name}")
+        create_materialised_view(short_name, force=True)
+
+    logging.info("All materialised views (re-)created")
+
+
+if __name__ == "__main__":
+    logging.basicConfig(level=logging.INFO)
+    recreate_materialised_views()
