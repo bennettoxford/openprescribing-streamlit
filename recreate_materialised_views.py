0ffb70e8b1268acf17bf3c3f529f53eb8341e75a
Richard Croker <ra.croker@gmail.com>
2026-05-25 14:47:25 +0100

    amend recreate_materialised_views to take into account new structure

--- recreate_materialised_views.py
+++ recreate_materialised_views.py
@@ -1,12 +1,15 @@
 import logging
+from pathlib import Path
 
 from db import (
     create_materialised_view,
     materialised_views_db_path,
-    materialised_views_dir,
 )
 
 logger = logging.getLogger(__name__)
+
+BASE_DIR = Path(__file__).parent
+APPS_DIR = BASE_DIR / "apps"
 
 
 def recreate_materialised_views():
@@ -15,10 +18,25 @@ def recreate_materialised_views():
         materialised_views_db_path.unlink()
 
     logging.debug("Creating all materialised views")
-    for f in sorted(materialised_views_dir.iterdir()):
-        short_name = f.name.removesuffix(".sql")
-        logging.debug(f"Creating materialised view {short_name}")
-        create_materialised_view(short_name, force=True)
+    for app_dir in sorted(APPS_DIR.iterdir()):
+        if not app_dir.is_dir():
+            continue
+
+        app_file = app_dir / "app.py"
+        materialised_views_dir = app_dir / "materialised_views"
+
+        if not app_file.exists() or not materialised_views_dir.is_dir():
+            continue
+
+        for f in sorted(materialised_views_dir.iterdir()):
+            short_name = f.name.removesuffix(".sql")
+            logging.debug(f"Creating materialised view {short_name}")
+            create_materialised_view(
+                short_name,
+                app_file,
+                app_dir.name,
+                force=True,
+            )
 
     logging.info("All materialised views (re-)created")
 
