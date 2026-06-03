883ddc247e84017f813b0ffa6d456e1e7ac4c86d
Richard Croker <ra.croker@gmail.com>
2026-06-03 19:49:35 +0100

    fix urls

--- hello.py
+++ hello.py
@@ -1,13 +1,13 @@
 import streamlit as st
 
 pages = st.navigation([
-    st.Page("pages/home.py", title="Home page"),
-    st.Page("apps/tariff_price_changes/tariff_price_changes.py", title="Tariff Price Changes"),
-    st.Page("apps/prescribing_topx/prescribing_topx.py", title="Top x Prescribing"),
-    st.Page("apps/measure_aware/measure_aware.py", title="aWaRe"),
-    st.Page("apps/opioids_ome/opioids_ome.py", title="Opioids OME"),
-    st.Page("pages/db_schema.py", title="Database schema"),
-    st.Page("pages/sql_checker.py", title="Code tests")
+    st.Page("pages/home.py",                        title="Home page"),
+    st.Page("apps/tariff_price_changes/app.py",     title="Tariff Price Changes",  url_path="tariff-price-changes"),
+    st.Page("apps/prescribing_topx/app.py",         title="Top x Prescribing",     url_path="prescribing-topx"),
+    st.Page("apps/measure_aware/app.py",            title="aWaRe",                 url_path="measure-aware"),
+    st.Page("apps/measure_ome/app.py",              title="Opioids OME",           url_path="measure-ome"),
+    st.Page("pages/db_schema.py",                   title="Database schema"),
+    st.Page("pages/sql_checker.py",                 title="Code tests")
 ])
 
 pages.run()
